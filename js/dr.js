24b83b69f0139512c7f9692cd1b17aa7bd72d20e
Katie Jergens <katie.jergens@gmail.com>
2018-05-02 10:01:48 -0400

    clickable image

--- js/dr.js
+++ js/dr.js
@@ -1,1 +1,59 @@
-var _0x2ce1=["\x74\x6F\x55\x70\x70\x65\x72\x43\x61\x73\x65","\x76\x61\x6C\x75\x65","\x73\x65\x61\x72\x63\x68\x2D\x74\x65\x72\x6D","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x6F\x70\x61\x63\x69\x74\x79","\x73\x74\x79\x6C\x65","\x72\x65\x73\x75\x6C\x74","\x4D\x4B\x53\x41\x49\x4C\x54\x47\x4C\x4C\x46\x56","\x69\x6E\x64\x65\x78\x4F\x66","\x4D\x4D\x4B\x53\x41\x49\x4C\x54\x47\x4C\x4C\x46\x56","\x73\x6F\x72\x72\x79\x2D\x6D\x65\x73\x73\x61\x67\x65","\x68\x72\x65\x66","\x72\x65\x73\x75\x6C\x74\x2D\x6C\x69\x6E\x6B","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x75\x6E\x69\x70\x72\x6F\x74\x2E\x6F\x72\x67\x2F\x75\x6E\x69\x70\x72\x6F\x74\x2F\x50\x31\x32\x34\x31\x37","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x54\x79\x70\x65\x2D\x33\x20\x69\x63\x65\x2D\x73\x74\x72\x75\x63\x74\x75\x72\x69\x6E\x67\x20\x70\x72\x6F\x74\x65\x69\x6E\x20\x31\x2E\x35","\x73\x72\x63","\x70\x72\x6F\x74\x65\x69\x6E\x2D\x69\x6D\x61\x67\x65","\x69\x6D\x67\x2F\x31\x2E\x6A\x70\x67","\x4D\x53\x4B\x47\x45\x45\x4C\x46\x54\x47\x56\x56","\x4D\x4D\x53\x4B\x47\x45\x45\x4C\x46\x54\x47\x56\x56","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x75\x6E\x69\x70\x72\x6F\x74\x2E\x6F\x72\x67\x2F\x75\x6E\x69\x70\x72\x6F\x74\x2F\x50\x34\x32\x32\x31\x32","\x47\x72\x65\x65\x6E\x20\x66\x6C\x75\x6F\x72\x65\x73\x63\x65\x6E\x74\x20\x70\x72\x6F\x74\x65\x69\x6E","\x69\x6D\x67\x2F\x32\x67\x66\x70\x2E\x70\x6E\x67","\x4D\x45\x4E\x4D\x45\x4E\x44\x45\x4E\x49\x56","\x4D\x4D\x45\x4E\x4D\x45\x4E\x44\x45\x4E\x49\x56","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x75\x6E\x69\x70\x72\x6F\x74\x2E\x6F\x72\x67\x2F\x75\x6E\x69\x70\x72\x6F\x74\x2F\x51\x30\x31\x31\x35\x38","\x4C\x75\x63\x69\x66\x65\x72\x69\x6E\x20\x34\x2D\x6D\x6F\x6E\x6F\x6F\x78\x79\x67\x65\x6E\x61\x73\x65","\x69\x6D\x67\x2F\x33\x6C\x75\x63\x69\x66\x65\x72\x6E\x2E\x70\x6E\x67","\x4D\x4B\x4C\x4E\x4B\x4C\x4E\x50\x52\x57\x44\x41","\x4D\x4D\x4B\x4C\x4E\x4B\x4C\x4E\x50\x52\x57\x44\x41","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x75\x6E\x69\x70\x72\x6F\x74\x2E\x6F\x72\x67\x2F\x75\x6E\x69\x70\x72\x6F\x74\x2F\x51\x38\x57\x54\x45\x37","\x4F\x64\x6F\x72\x61\x6E\x74\x20\x72\x65\x63\x65\x70\x74\x6F\x72\x20\x4F\x72\x31","\x69\x6D\x67\x2F\x34\x6F\x6C\x66\x61\x63\x74\x6F\x72\x79\x2E\x67\x69\x66","\x4D\x4B\x54\x4C\x4C\x4C\x54\x4C\x56\x56\x56\x54","\x4D\x4D\x4B\x54\x4C\x4C\x4C\x54\x4C\x56\x56\x56\x54","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x75\x6E\x69\x70\x72\x6F\x74\x2E\x6F\x72\x67\x2F\x75\x6E\x69\x70\x72\x6F\x74\x2F\x51\x37\x54\x33\x4A\x32","\x41\x6C\x70\x68\x61\x2D\x65\x6C\x61\x70\x69\x74\x6F\x78\x69\x6E\x2D\x42\x6D\x32\x61","\x69\x6D\x67\x2F\x35\x61\x62\x74\x2E\x70\x6E\x67","\x4D\x41\x45\x50\x41\x4B\x4B\x4B\x50\x4B\x4B\x4C","\x4D\x4D\x41\x45\x50\x41\x4B\x4B\x4B\x50\x4B\x4B\x4C","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x75\x6E\x69\x70\x72\x6F\x74\x2E\x6F\x72\x67\x2F\x75\x6E\x69\x70\x72\x6F\x74\x2F\x51\x39\x39\x32\x38\x35","\x48\x69\x73\x74\x6F\x6E\x65\x20\x48\x32\x42","\x69\x6D\x67\x2F\x36\x68\x62\x2E\x70\x6E\x67","\x4D\x41\x44\x4B\x56\x4C\x4B\x45\x4B\x52","\x4D\x4D\x41\x44\x4B\x56\x4C\x4B\x45\x4B\x52","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x75\x6E\x69\x70\x72\x6F\x74\x2E\x6F\x72\x67\x2F\x75\x6E\x69\x70\x72\x6F\x74\x2F\x50\x32\x39\x34\x36\x36","\x43\x61\x73\x70\x61\x73\x65\x2D\x31","\x69\x6D\x67\x2F\x37\x63\x61\x70\x2E\x70\x6E\x67","\x4D\x45\x46\x54\x4C\x52\x51\x45\x41\x4C","\x4D\x4D\x45\x46\x54\x4C\x52\x51\x45\x41\x4C","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x75\x6E\x69\x70\x72\x6F\x74\x2E\x6F\x72\x67\x2F\x75\x6E\x69\x70\x72\x6F\x74\x2F\x50\x39\x33\x38\x34\x38","\x4C\x65\x67\x68\x65\x6D\x6F\x67\x6C\x6F\x62\x69\x6E\x20\x32\x39","\x69\x6D\x67\x2F\x38\x6C\x68\x67\x2E\x70\x6E\x67","","\x63\x6C\x69\x63\x6B","\x23\x73\x75\x62\x6D\x69\x74"];$(_0x2ce1[56])[_0x2ce1[55]](function(){var _0x7087x1=String(document[_0x2ce1[3]](_0x2ce1[2])[_0x2ce1[1]])[_0x2ce1[0]]();document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 0;if(_0x7087x1[_0x2ce1[8]](_0x2ce1[7])=== 0|| _0x7087x1[_0x2ce1[8]](_0x2ce1[9])=== 0){document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 1;document[_0x2ce1[3]](_0x2ce1[10])[_0x2ce1[5]][_0x2ce1[4]]= 0;document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[11]]= _0x2ce1[13];document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[14]]= _0x2ce1[15];document[_0x2ce1[3]](_0x2ce1[17])[_0x2ce1[16]]= _0x2ce1[18]}else {if(_0x7087x1[_0x2ce1[8]](_0x2ce1[19])=== 0|| _0x7087x1[_0x2ce1[8]](_0x2ce1[20])=== 0){document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 1;document[_0x2ce1[3]](_0x2ce1[10])[_0x2ce1[5]][_0x2ce1[4]]= 0;document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[11]]= _0x2ce1[21];document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[14]]= _0x2ce1[22];document[_0x2ce1[3]](_0x2ce1[17])[_0x2ce1[16]]= _0x2ce1[23]}else {if(_0x7087x1[_0x2ce1[8]](_0x2ce1[24])=== 0|| _0x7087x1[_0x2ce1[8]](_0x2ce1[25])=== 0){document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 1;document[_0x2ce1[3]](_0x2ce1[10])[_0x2ce1[5]][_0x2ce1[4]]= 0;document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[11]]= _0x2ce1[26];document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[14]]= _0x2ce1[27];document[_0x2ce1[3]](_0x2ce1[17])[_0x2ce1[16]]= _0x2ce1[28]}else {if(_0x7087x1[_0x2ce1[8]](_0x2ce1[29])=== 0|| _0x7087x1[_0x2ce1[8]](_0x2ce1[30])=== 0){document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 1;document[_0x2ce1[3]](_0x2ce1[10])[_0x2ce1[5]][_0x2ce1[4]]= 0;document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[11]]= _0x2ce1[31];document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[14]]= _0x2ce1[32];document[_0x2ce1[3]](_0x2ce1[17])[_0x2ce1[16]]= _0x2ce1[33]}else {if(_0x7087x1[_0x2ce1[8]](_0x2ce1[34])=== 0|| _0x7087x1[_0x2ce1[8]](_0x2ce1[35])=== 0){document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 1;document[_0x2ce1[3]](_0x2ce1[10])[_0x2ce1[5]][_0x2ce1[4]]= 0;document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[11]]= _0x2ce1[36];document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[14]]= _0x2ce1[37];document[_0x2ce1[3]](_0x2ce1[17])[_0x2ce1[16]]= _0x2ce1[38]}else {if(_0x7087x1[_0x2ce1[8]](_0x2ce1[39])=== 0|| _0x7087x1[_0x2ce1[8]](_0x2ce1[40])=== 0){document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 1;document[_0x2ce1[3]](_0x2ce1[10])[_0x2ce1[5]][_0x2ce1[4]]= 0;document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[11]]= _0x2ce1[41];document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[14]]= _0x2ce1[42];document[_0x2ce1[3]](_0x2ce1[17])[_0x2ce1[16]]= _0x2ce1[43]}else {if(_0x7087x1[_0x2ce1[8]](_0x2ce1[44])=== 0|| _0x7087x1[_0x2ce1[8]](_0x2ce1[45])=== 0){document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 1;document[_0x2ce1[3]](_0x2ce1[10])[_0x2ce1[5]][_0x2ce1[4]]= 0;document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[11]]= _0x2ce1[46];document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[14]]= _0x2ce1[47];document[_0x2ce1[3]](_0x2ce1[17])[_0x2ce1[16]]= _0x2ce1[48]}else {if(_0x7087x1[_0x2ce1[8]](_0x2ce1[49])=== 0|| _0x7087x1[_0x2ce1[8]](_0x2ce1[50])=== 0){document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 1;document[_0x2ce1[3]](_0x2ce1[10])[_0x2ce1[5]][_0x2ce1[4]]= 0;document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[11]]= _0x2ce1[51];document[_0x2ce1[3]](_0x2ce1[12])[_0x2ce1[14]]= _0x2ce1[52];document[_0x2ce1[3]](_0x2ce1[17])[_0x2ce1[16]]= _0x2ce1[53]}else {document[_0x2ce1[3]](_0x2ce1[6])[_0x2ce1[5]][_0x2ce1[4]]= 0;document[_0x2ce1[3]](_0x2ce1[10])[_0x2ce1[5]][_0x2ce1[4]]= 1;document[_0x2ce1[3]](_0x2ce1[17])[_0x2ce1[16]]= _0x2ce1[54]}}}}}}}}})
+ $("#submit").click(function() {
+    var searchterm = String(document.getElementById("search-term").value).toUpperCase();
+
+   document.getElementById("result").style.opacity = 0; // make it flash off and on
+
+    if (searchterm.indexOf("MKSAILTGLLFV") === 0 || searchterm.indexOf("MMKSAILTGLLFV") === 0) {
+        document.getElementById("result").style.opacity = 1;
+        document.getElementById("sorry-message").style.opacity = 0;
+        $(".protein-link").attr("href", "http://www.uniprot.org/uniprot/P12417"); 
+        document.getElementById("result-link").innerHTML = "Type-3 ice-structuring protein 1.5";
+        document.getElementById("protein-image").src = "img/1.jpg";
+    } else if (searchterm.indexOf("MSKGEELFTGVV") === 0 || searchterm.indexOf("MMSKGEELFTGVV") === 0) {
+        document.getElementById("result").style.opacity = 1;
+        document.getElementById("sorry-message").style.opacity = 0;
+        $(".protein-link").attr("href", "http://www.uniprot.org/uniprot/P42212");
+        document.getElementById("result-link").innerHTML = "Green fluorescent protein";
+        document.getElementById("protein-image").src = "img/2gfp.png";
+    } else if (searchterm.indexOf("MENMENDENIV") === 0 || searchterm.indexOf("MMENMENDENIV") === 0) {
+        document.getElementById("result").style.opacity = 1;
+        document.getElementById("sorry-message").style.opacity = 0;
+        $(".protein-link").attr("href", "http://www.uniprot.org/uniprot/Q01158");
+        document.getElementById("result-link").innerHTML = "Luciferin 4-monooxygenase";
+        document.getElementById("protein-image").src = "img/3lucifern.png";
+    } else if (searchterm.indexOf("MKLNKLNPRWDA") === 0 || searchterm.indexOf("MMKLNKLNPRWDA") === 0) {
+        document.getElementById("result").style.opacity = 1;
+        document.getElementById("sorry-message").style.opacity = 0;
+        $(".protein-link").attr("href", "http://www.uniprot.org/uniprot/Q8WTE7");
+        document.getElementById("result-link").innerHTML = "Odorant receptor Or1";
+        document.getElementById("protein-image").src = "img/4olfactory.gif";
+    } else if (searchterm.indexOf("MKTLLLTLVVVT") === 0 || searchterm.indexOf("MMKTLLLTLVVVT") === 0) {
+        document.getElementById("result").style.opacity = 1;
+        document.getElementById("sorry-message").style.opacity = 0;
+        d$(".protein-link").attr("href", "http://www.uniprot.org/uniprot/Q7T3J2");
+        document.getElementById("result-link").innerHTML = "Alpha-elapitoxin-Bm2a";
+        document.getElementById("protein-image").src = "img/5abt.png";
+    } else if (searchterm.indexOf("MAEPAKKKPKKL") === 0 || searchterm.indexOf("MMAEPAKKKPKKL") === 0) {
+        document.getElementById("result").style.opacity = 1;
+        document.getElementById("sorry-message").style.opacity = 0;
+        $(".protein-link").attr("href", "http://www.uniprot.org/uniprot/Q99285");
+        document.getElementById("result-link").innerHTML = "Histone H2B";
+        document.getElementById("protein-image").src = "img/6hb.png";
+    } else if (searchterm.indexOf("MADKVLKEKR") === 0 || searchterm.indexOf("MMADKVLKEKR") === 0) {
+        document.getElementById("result").style.opacity = 1;
+        document.getElementById("sorry-message").style.opacity = 0;
+        $(".protein-link").attr("href", "http://www.uniprot.org/uniprot/P29466");
+        document.getElementById("result-link").innerHTML = "Caspase-1";
+        document.getElementById("protein-image").src = "img/7cap.png";
+    } else if (searchterm.indexOf("MEFTLRQEAL") ===  0 || searchterm.indexOf("MMEFTLRQEAL")===  0) {
+        document.getElementById("result").style.opacity = 1;
+        document.getElementById("sorry-message").style.opacity = 0;
+        $(".protein-link").attr("href", "http://www.uniprot.org/uniprot/P93848");
+        document.getElementById("result-link").innerHTML = "Leghemoglobin 29";
+        document.getElementById("protein-image").src = "img/8lhg.png";
+    } else {
+        document.getElementById("result").style.opacity = 0; 
+        document.getElementById("sorry-message").style.opacity = 1; 
+        document.getElementById("protein-image").src = "";
+  } 
+});
